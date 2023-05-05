86af88a863db2b8b84569a60d88d1647773a8357
Patrick <74751873+PatrickCuentas@users.noreply.github.com>
2023-05-05 00:13:54 -0500

    ci: :wrench: se actualizo la url de la api

--- lib/shared/globals.dart
+++ lib/shared/globals.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/animation.dart';
 
 class Globals {
-  // la IPV4 debe ser la de tu computadora usando ipconfig desde el cmd
-  static const baseApiUrl = 'http://192.168.0.12:8080/api/v1';
+  static const baseApiUrl = 'http://46.101.207.207:8080/api/v1';
   static Color primaryColor = const Color.fromARGB(255, 22, 29, 47);
 }
